515d4ce59c215dcd4b6614278fe987b961b9a4a4
Jon Larkowski and Matthew Conway <dev+l4rk+mattonrails@hashrocket.com>
2010-11-09 11:19:47 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+
+gem 'pg', '0.9.0'
+gem 'rails', '3.0.1'
+
+group :development do
+  gem 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchBunch::Application.config.secret_token = 'c4d26dab252e2e3c87cfdfa8ecc6d05e03474571c069dafac806d3b0957e4ca2f8d735818ea0c6c434879103cda30586eb998fb09f1f5145ba5d801304ff11da'
