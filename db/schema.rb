b787a93a9a2c2c154a7d22f6a795f92685f71aac
Matthew Conway <matt.conway@hashrocket.com>
2010-11-16 11:46:09 -0500

    Add name to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101109213453) do
+ActiveRecord::Schema.define(:version => 20101116141506) do
 
   create_table "outings", :force => true do |t|
     t.integer  "restaurant_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20101109213453) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
